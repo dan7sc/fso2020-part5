cba7663176e3d6bca118b2497c57c429851c3dc1
dan7sc <su7cheng@gmail.com>
2020-05-10 12:20:57 -0300

    Add solution to exercise 5.18: bloglist end to end testing, step2

--- cypress/integration/bloglist_app.spec.js
+++ cypress/integration/bloglist_app.spec.js
@@ -1,6 +1,12 @@
 describe('Bloglist app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -8,5 +14,25 @@ describe('Bloglist app', function() {
     cy.contains('log in to application')
     cy.contains('username')
     cy.contains('password')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+
+      cy.contains('Matti Luukkainen logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('wrongpass')
+      cy.contains('login').click()
+
+      cy.get('.error')
+        .should('contain', 'wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
